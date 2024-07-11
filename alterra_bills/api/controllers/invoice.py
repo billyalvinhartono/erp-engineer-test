e986a6f6cc4743d8b161b935c1b35165475b3f16
Billy Alv <billy@erpindonesia.co.id>
2024-07-11 15:31:18 +0700

    build(init) add, update  invoice and payment endpoint
    - add require and opened update list for create invoice
    - add function availibility
    - add open parameter for invoice list that still open
    - add payment list based on invoice
    - add create invoice endpoint single or bulk
    - add register payment endpoint single, bulk or multiple invoice in one payment or group payment. 
    
    Update invoice.py and payment.py

--- alterra_bills/api/controllers/invoice.py
+++ alterra_bills/api/controllers/invoice.py
@@ -4,11 +4,66 @@ import logging
 
 from odoo.addons.alterra_bills.api.controllers.rest import Rest
 _logger = logging.getLogger(__name__)
+import json
+
+INVOICE_REQUIRE_CREATE = ['invoice_date','partner_id','invoice_line_ids']
+INVOICE_LINE_REQUIRE_CREATE = ['product_id','label','quantity','price_unit']
+INVOICE_OPEN_UPDATE = ['invoice_date','partner_id','ref','invoice_line_ids']
+INVOICE_LINE_OPEN_UPDATE = ['product_id','label','quantity','price_unit']
 
 class Invoice(http.Controller):
+
+	#checking completence of data
+	def create_data_availability(self,kwargs):
+
+		# invoice required fields
+		model_id = request.env['ir.model'].sudo().search([('model','=','account.move')])
+		for item in INVOICE_REQUIRE_CREATE:
+			if not kwargs.get(item):
+				return item +' can not be empty!'
+
+		# handling no fields on invoice
+		model_id = request.env['ir.model'].sudo().search([('model','=','account.move')])
+		for key, value in kwargs.items():
+			if not request.env['ir.model.fields'].sudo().search([('model_id','=',model_id.id),('name','=',key)]).id:
+				return 'No field named : '+key
+
+		if not request.env['res.partner'].search([('id','=',kwargs.get('partner_id'))]).id:
+			return 'partner_id not found!'
+		
+		# invoice line required fields
+		for item in INVOICE_LINE_REQUIRE_CREATE:
+			for line in kwargs['invoice_line_ids']:
+				if not line.get(item):
+					return item +' can not be empty!'
+		for line in kwargs['invoice_line_ids']:
+			if not request.env['product.product'].search([('id','=',line.get('product_id'))]).id:
+				return 'product_id not found!'
+		return 'Success'
 	
+	def update_data_availability(self,kwargs):
+		# check data availibility
+		model_id = request.env['ir.model'].sudo().search([('model','=','account.move')])
+		fields = model_id.field_id.mapped('name')
+		for key, value in kwargs.items():
+			print(key)
+			if key not in fields:
+				return 'No field named : '+key
+			if key not in INVOICE_OPEN_UPDATE:
+				print(INVOICE_OPEN_UPDATE)
+				return 'Your are not allowed to update '+key
+		
+		if kwargs.get('invoice_line_ids'):
+			for line in kwargs.get('invoice_line_ids'):
+				for key_line, value_line in line.items():
+					if key_line not in INVOICE_LINE_OPEN_UPDATE:
+						return 'Your are not allowed to update '+key_line
+
+		return 'Success'
+
+	# Invoice List Endpoint
 	@http.route(['/invoice/list'], type='json', auth='user')
-	def invoice_list(self,invoice_number=False):
+	def invoice_list(self,invoice_number=False,open=False):
 		api = "invoice:list"
 		noaccess = False
 		# Request
@@ -18,13 +73,27 @@ class Invoice(http.Controller):
 				noaccess = True
 				raise Exception('You do not have access!')
 
+			#create domain
 			domain = [('move_type','=','out_invoice')]
 			if invoice_number:
 				domain.append(('name','ilike',invoice_number))
+			if open:
+				domain.append(('payment_state','in',['not_paid','partial']))
 
+			#search all invoice and append to an array
 			move_ids = request.env['account.move'].sudo().search(domain, order="id asc")
 			data = []
 			for move in move_ids:
+				# add payments
+				payments = []
+				for payment_id in move._get_reconciled_info_JSON_values():
+					payments.append({
+						'id': payment_id['account_payment_id'],
+						'name': payment_id['name'],
+						'date': payment_id['date'],
+						'amount': payment_id['amount'],
+					})
+				# add lines
 				lines = []
 				for line in move.invoice_line_ids:
 					taxes = []
@@ -39,20 +108,190 @@ class Invoice(http.Controller):
 						'product_id': line.product_id.id,
 						'product_name': line.product_id.name,
 						'label': line.name,
-						'qty': line.quantity,
+						'quantity': line.quantity,
 						'price_unit': line.price_unit,
 						'taxes': taxes,
 						'price_subtotal': line.price_subtotal,
 					})
+				#assign return and log
 				data.append({
+					'id': move.id,
 					'name': move.name,
+					'invoice_date': move.invoice_date,
+					'amount_total': move.amount_total,
+					'amount_residual': move.amount_residual,
+					'payment_state': move.payment_state,
 					'partner_id': move.partner_id.id,
 					'partner_name': move.partner_id.name,
 					'partner_street': move.partner_id.street,
-					'move_line': lines,
+					'invoice_line_ids': lines,
+					'payment_ids': payments,
 					})
-
+	
 			return Rest.response(self,data,api)
-		# Error exception #################################################
+		# Error exception
+		except Exception as error:
+			return Rest.exception(self,noaccess,error,api)
+
+	# Invoice Create Endpoint
+	@http.route(['/invoice/create'], type='json', auth='user')
+	def invoice_create(self,**kwargs):
+		api = "invoice:create"
+		noaccess = False
+		# Request
+		try:
+			# Check user access
+			if not request.env.user.is_api:
+				noaccess = True
+				raise Exception('You do not have access!')
+			
+			#check data
+			valid = self.create_data_availability(kwargs)
+			if valid != 'Success':
+				raise Exception(valid)
+			
+			#assign value
+			vals = {
+				'partner_id': kwargs['partner_id'],
+				'invoice_date': kwargs['invoice_date'],
+				'move_type': 'out_invoice',
+			}
+
+			#assign value line
+			lines = []
+			for line in kwargs['invoice_line_ids']:
+				taxes =[]
+				if line.get('tax_ids'):
+					for tax_id in line['tax_ids']:
+						taxes.append(tax_id)
+
+				vals_line = {
+					'product_id': line['product_id'],
+					'name': line['label'],
+					'quantity': line['quantity'],
+					'tax_ids': taxes,
+					'price_unit': line['price_unit'],
+				}
+				lines.append((0,0,vals_line))
+			vals['invoice_line_ids'] = lines
+
+			# create invoice
+			move_id = request.env['account.move'].sudo().create(vals)
+			move_id.action_post()
+
+			#assign return and log
+			data = {
+				'name': move_id.name,
+			}
+			
+			return Rest.response(self,data,api)
+		# Error exception
+		except Exception as error:
+			return Rest.exception(self,noaccess,error,api)
+
+
+	# Invoice Create Bulk Endpoint
+	@http.route(['/invoice/create-bulk'], type='json', auth='user')
+	def invoice_create_bulk(self,**kwargs):
+		api = "invoice:create-bulk"
+		noaccess = False
+		# Request
+		try:
+			# Check user access
+			if not request.env.user.is_api:
+				noaccess = True
+				raise Exception('You do not have access!')
+			data = []
+			vals_list = []
+			for move in kwargs['data']:
+				#check data
+				valid = self.create_data_availability(move)
+				if valid != 'Success':
+					raise Exception(valid)
+				
+				#assign value
+				vals = {
+					'partner_id': move['partner_id'],
+					'invoice_date': move['invoice_date'],
+					'move_type': 'out_invoice',
+				}
+
+				#assign value line
+				lines = []
+				for line in move['invoice_line_ids']:
+					vals_line = {
+						'product_id': line['product_id'],
+						'name': line['label'],
+						'quantity': line['quantity'],
+						'price_unit': line['price_unit'],
+					}
+					lines.append((0,0,vals_line))
+				vals['invoice_line_ids'] = lines
+				vals_list.append(vals)
+
+			
+			# create invoice
+			move_ids = request.env['account.move'].sudo().create(vals_list)
+			move_ids.action_post()
+
+			for move_id in move_ids:
+				#assign return and log
+				data.append({'name': move_id.name})
+			
+			return Rest.response(self,data,api)
+		# Error exception
+		except Exception as error:
+			return Rest.exception(self,noaccess,error,api)
+	
+	# Invoice Update Endpoint
+	@http.route(['/invoice/update'], type='json', auth='user')
+	def invoice_update(self,id,**kwargs):
+		api = "invoice:update"
+		noaccess = False
+		# Request
+		try:
+			# Check user access
+			if not request.env.user.is_api:
+				noaccess = True
+				raise Exception('You do not have access!')
+			
+			#check data
+			valid = self.update_data_availability(kwargs)
+			if valid != 'Success':
+				raise Exception(valid)
+			
+			move_id = request.env['account.move'].sudo().search([('id','=',id)])
+			if not move_id.id:
+				raise Exception('Invoice id not found!')
+			
+			#update invoice
+			move_id.button_draft()
+			vals ={}
+			for item in INVOICE_OPEN_UPDATE:
+				if kwargs.get(item):
+					if item == 'invoice_line_ids':
+						move_id.invoice_line_ids = [(5, 0, 0)]
+						vals_line = []
+						for line in kwargs['invoice_line_ids']:
+							vals_line.append((0,0,{
+								'product_id': line['product_id'],
+								'name': line['label'],
+								'quantity': line['quantity'],
+								'price_unit': line['price_unit'],
+							}))
+						vals['invoice_line_ids'] = vals_line
+					else:
+						vals[item] = kwargs[item]
+
+			move_id.update(vals)
+			move_id.action_post()
+
+			#assign return and log
+			data = {
+				'name': move_id.name,
+			}
+			
+			return Rest.response(self,data,api)
+		# Error exception
 		except Exception as error:
 			return Rest.exception(self,noaccess,error,api)
--- alterra_bills/api/controllers/payment.py
+++ alterra_bills/api/controllers/payment.py
@@ -39,3 +39,160 @@ class Invoice(http.Controller):
 		# Error exception #################################################
 		except Exception as error:
 			return Rest.exception(self,noaccess,error,api)
+	
+	# payment register Endpoint
+	@http.route(['/payment/register'], type='json', auth='user')
+	def invoice_register(self,id,amount=False,payment_date=False,**kwargs):
+		api = "payment:register"
+		noaccess = False
+		# Request
+		try:
+			# Check user access
+			if not request.env.user.is_api:
+				noaccess = True
+				raise Exception('You do not have access!')
+			
+			#check data validity
+			move_id = request.env['account.move'].sudo().search([
+				('state','=','posted'),
+				('move_type','=','out_invoice'),
+				('id','=',id)])
+			if not move_id.id:
+				raise Exception('invoice id not found!')
+			if move_id.payment_state == 'paid':
+				raise Exception('invoice already paid!')
+
+			vals = {}
+			if amount:
+				if amount == 0 or amount < 0:
+					raise Exception('amount can not be 0 or under 0!')
+				if amount > move_id.amount_residual:
+					raise Exception('amount can not be more than residual value! residual value is '+ str(move_id.amount_residual))
+				else:
+					vals['amount'] = amount
+			if payment_date:
+				vals['payment_date'] = payment_date
+
+			#create wizard enviroment and register payment
+			context= {'active_model': 'account.move','active_ids': move_id.ids,}
+			wiz_id = request.env['account.payment.register'].with_context(context).sudo().create(vals)
+			payment_ids = wiz_id._create_payments()
+			
+			for payment_id in payment_ids:
+				#assign return and log
+				data = {
+					'payment_number': payment_id.name,
+				}
+			
+			return Rest.response(self,data,api)
+		# Error exception
+		except Exception as error:
+			return Rest.exception(self,noaccess,error,api)
+	
+	# payment register bulk Endpoint
+	@http.route(['/payment/register-bulk'], type='json', auth='user')
+	def invoice_register_bulk(self,data,**kwargs):
+		api = "payment:register-bulk"
+		noaccess = False
+		# Request
+		try:
+			# Check user access
+			if not request.env.user.is_api:
+				noaccess = True
+				raise Exception('You do not have access!')
+			
+			resp=[]
+			for invoice_id in data:
+				#check data validity
+				move_id = request.env['account.move'].sudo().search([
+					('state','=','posted'),
+					('move_type','=','out_invoice'),
+					('id','=',invoice_id['id'])])
+				if not move_id.id:
+					raise Exception('invoice for id ' + str(move_id.id) + '(' + move_id.name + ') id not found!')
+				if move_id.payment_state == 'paid':
+					raise Exception('invoice for id ' + str(move_id.id) + '(' + move_id.name + ') already paid!')
+
+				vals = {}
+				if invoice_id['amount']:
+					if invoice_id['amount'] == 0 or invoice_id['amount'] < 0:
+						raise Exception('amount for id ' + str(move_id.id) + '(' + move_id.name + ') can not be 0 or under 0!')
+					if invoice_id['amount'] > move_id.amount_residual:
+						raise Exception('amount can not be more than residual value! residual value for id ' + str(move_id.id) + '(' + move_id.name + ') is '+ str(move_id.amount_residual))
+					else:
+						vals['amount'] = invoice_id['amount']
+				if invoice_id.get('payment_date'):
+					vals['payment_date'] = invoice_id['payment_date']
+
+				#create wizard enviroment and register payment
+				context= {'active_model': 'account.move','active_ids': move_id.ids,}
+				wiz_id = request.env['account.payment.register'].with_context(context).sudo().create(vals)
+				payment_ids = wiz_id._create_payments()
+				for payment_id in payment_ids:
+					#assign return and log
+					resp.append({
+						'payment_number': payment_id.name,
+					})
+			
+			data = resp
+			return Rest.response(self,data,api)
+		# Error exception
+		except Exception as error:
+			return Rest.exception(self,noaccess,error,api)
+
+	# payment register merge Endpoint
+	@http.route(['/payment/register-group'], type='json', auth='user')
+	def invoice_register_merge(self,invoice_ids,amount=False,payment_date=False,**kwargs):
+		api = "payment:register-merge"
+		noaccess = False
+		# Request
+		try:
+			# Check user access
+			if not request.env.user.is_api:
+				noaccess = True
+				raise Exception('You do not have access!')
+			
+			resp=[]
+			move_ids = request.env['account.move'].sudo().search([
+					('state','=','posted'),
+					('move_type','=','out_invoice'),
+					('id','in',invoice_ids)])
+			
+			if len(move_ids.mapped('partner_id')) > 1:
+				raise Exception('All invoice should be in the same customer!')
+
+			for move_id in move_ids:
+				#check data validity
+				if not move_id.id:
+					raise Exception('invoice for id ' + str(move_id.id) + '(' + move_id.name + ') id not found!')
+				if move_id.payment_state == 'paid':
+					raise Exception('invoice for id ' + str(move_id.id) + '(' + move_id.name + ') already paid!')
+
+			vals = {}
+			if amount:
+				if amount == 0 or amount < 0:
+					raise Exception('amount can not be 0 or under 0!')
+				if amount > sum(move_ids.mapped('amount_residual')):
+					raise Exception('amount can not be more than residual value! residual value is '+ str(sum(move_ids.mapped('amount_residual'))))
+				else:
+					vals['amount'] = amount
+
+			vals['group_payment'] = True
+			if payment_date:
+				vals['payment_date'] = payment_date
+
+			#create wizard enviroment and register payment
+			context= {'active_model': 'account.move','active_ids': move_ids.ids,}
+			wiz_id = request.env['account.payment.register'].with_context(context).sudo().create(vals)
+			payment_ids = wiz_id._create_payments()
+			for payment_id in payment_ids:
+				#assign return and log
+				resp.append({
+					'payment_number': payment_id.name,
+				})
+			
+			data = resp
+			return Rest.response(self,data,api)
+		# Error exception
+		except Exception as error:
+			return Rest.exception(self,noaccess,error,api)
